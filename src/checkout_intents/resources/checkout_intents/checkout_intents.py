5c45e1804fe1173a25f299c9e003ec15f6a17543
MAdisrya <mario.adisurya@gmail.com>
2026-03-07 12:57:02 +1300

    fix(internal): duplicate definitions

--- src/checkout_intents/resources/checkout_intents/checkout_intents.py
+++ src/checkout_intents/resources/checkout_intents/checkout_intents.py
@@ -1268,10 +1268,6 @@ class CheckoutIntentsResourceWithRawResponse:
     def shipments(self) -> ShipmentsResourceWithRawResponse:
         return ShipmentsResourceWithRawResponse(self._checkout_intents.shipments)
 
-    @cached_property
-    def shipments(self) -> ShipmentsResourceWithRawResponse:
-        return ShipmentsResourceWithRawResponse(self._checkout_intents.shipments)
-
 
 class AsyncCheckoutIntentsResourceWithRawResponse:
     def __init__(self, checkout_intents: AsyncCheckoutIntentsResource) -> None:
@@ -1312,10 +1308,6 @@ class AsyncCheckoutIntentsResourceWithRawResponse:
     def shipments(self) -> AsyncShipmentsResourceWithRawResponse:
         return AsyncShipmentsResourceWithRawResponse(self._checkout_intents.shipments)
 
-    @cached_property
-    def shipments(self) -> AsyncShipmentsResourceWithRawResponse:
-        return AsyncShipmentsResourceWithRawResponse(self._checkout_intents.shipments)
-
 
 class CheckoutIntentsResourceWithStreamingResponse:
     def __init__(self, checkout_intents: CheckoutIntentsResource) -> None:
@@ -1351,10 +1343,6 @@ class CheckoutIntentsResourceWithStreamingResponse:
         self.confirm_and_poll = to_streamed_response_wrapper(
             checkout_intents.confirm_and_poll,
         )
-
-    @cached_property
-    def shipments(self) -> ShipmentsResourceWithStreamingResponse:
-        return ShipmentsResourceWithStreamingResponse(self._checkout_intents.shipments)
 
     @cached_property
     def shipments(self) -> ShipmentsResourceWithStreamingResponse:
